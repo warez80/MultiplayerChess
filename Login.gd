45ef89b06b1ee5927de81ff8c041568b3f8657fd
Chris Nastovski <chris@MacBook.local>
2018-03-20 20:07:34 -0400

    Added example login REST API call

---
+++ Login.gd
@@ -0,0 +1,26 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Button_pressed():	
+	var QUERY = "a=login&u=chris2&p=123&i=localhost"
+	var HEADERS = ["Content-Type: application/x-www-form-urlencoded", "Content-Length: " + str(QUERY.length())]
+	$HTTPRequest.request("http://www.chrisnastovski.com/COP4331/api.php", HEADERS, true, HTTPClient.METHOD_POST, QUERY)
+
+func _on_HTTPRequest_request_completed( result, response_code, headers, body ):
+    var json = JSON.parse(body.get_string_from_utf8())
+    print(json.result)
+
