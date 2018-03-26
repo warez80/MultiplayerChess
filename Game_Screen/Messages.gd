359babc39c0699e97516484eb1a4c00b92502bae
Bryan Medina <bjmedina.cs@gmail.com>
2018-03-26 00:56:42 -0400

    Can now type message into message box, and send to message list

--- Game_Screen/Messages.gd
+++ Game_Screen/Messages.gd
@@ -1,40 +1,33 @@
 extends VBoxContainer
+export(NodePath) var path
 
-# sample message list
 
-var messages = ["hello", "hey how are you?", "im not doing so well.", "why not?", "because u about to do die bc of this chess game"]
-var children = []
+var messages = []
+var message = ""
 
 func _ready():
-	print("container reader")
-	for i in range(0, 10*len(messages)):
-		children.append(Label.new())
-		children[i].text = messages[i % (len(messages))]
-		
-	#for i in range(0, len(children)):
-	#	children[i].text = messages[i % (len(messages))]
-
-
-func check_for_new_message():
-	# add new child to children if there are any new messages
 	pass
 
-func _process(delta):
+func add_new_message(message):
 	
-	# Check for new messages
-	check_for_new_message()
+	#Todo: Get username and append to the front of the message
 	
-	for i in range(0, len(children)):
-		add_child(children[i])
+	# create a new label
+	var to_add = Label.new()
+	to_add.text = message
 	
-	#var x = self.get_children();
-	#print("number of children: " + str(self.get_child_count()))
-	
-		
-	
+	# add new child to children if there are any new messages
+	messages.append(to_add)
 
+func print_message(message):
+	print("connected to Messages: " + message)
 	
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	# Check for new messages
+	var textedit = get_node("../../../Text_Input/TextEdit")
+	textedit.connect("sent", self, "add_new_message", [])
+	
+	# add children to VBoxcontainer
+	for i in range(0, len(messages)):
+		add_child(messages[i])
+	
--- Game_Screen/TextEdit.gd
+++ Game_Screen/TextEdit.gd
@@ -1,7 +1,10 @@
 extends TextEdit
+export(NodePath) var path
+
+signal sent(message)
 
 func _ready():
-	print("textbox ready")
+	pass
 
 func _process(delta):
 	var send_button = get_node("../Button")
@@ -9,9 +12,18 @@ func _process(delta):
 	
 func send_message():
 	if(self.text != ""):
-		# need to send message and then clear the textEdit box
-		pass
+		# select all the text
+		self.select_all()
 		
+		# selected text is going to be the text we send
+		var message = self.get_selection_text()
+		#print("From TextEdit. Message : " + message)
+		
+		# send a signal to "Messages" node to get message we send
+		emit_signal("sent", message)
+		
+		# send 
+		self.cut()
 	
 	
 #func _process(delta):
