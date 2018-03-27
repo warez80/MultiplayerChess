04d709535a22075a679780b3d107039e2f587cb6
Bryan Medina <bjmedina.cs@gmail.com>
2018-03-27 19:22:44 -0400

    added Space.gd for spaces on board

---
+++ Game_Screen/Space.gd
@@ -0,0 +1,40 @@
+extends GridContainer
+
+const row = 8
+const col = 8
+const width  = 123
+const height = 123
+
+var spaces = []
+var init = false
+
+func spaces_init():
+	for i in range(0, row):
+		for j in range(0, col):
+			# Each child will be a textrect, and we want 64 of them
+			var child = TextureRect.new()
+			spaces.append(child)
+			
+	for i in range(0, row):
+		for j in range(0, col):
+			self.add_child(spaces[i + j*row])
+			
+	for b in self.get_children():
+		print(b)
+	
+	init = true
+	
+func _ready():
+	# We only want to initial spaces array once. We will also repeatedly use this method
+	# for connecting to other nodes, so there is a risk of us adding more elements to 
+	# the spaces array. To avoid that, we have the init boolean
+	if(!init):
+		spaces_init()
+		
+	
+	
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
