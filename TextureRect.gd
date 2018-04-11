83a7e286e368a4dff99dec3c8cc5d6b6a276695e
bobacrazy <minh.pham@knights.ucf.edu>
2018-04-10 23:20:33 -0400

    Added lose screen.

---
+++ TextureRect.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	if (Input.is_action_pressed("ui_cancel")):
+		get_tree().quit()
+	elif (Input.is_action_pressed("ui_accept")):
+		get_tree().change_scene("res://LobbySearch.tscn")
+	
